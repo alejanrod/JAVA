64cd5ae89d36ef5ddcc23d80e2889e53bf6c0ead
alejanrod <111410298+alejanrod@users.noreply.github.com>
2023-03-06 19:38:32 -0500

    Update Personas.java

--- talleragregacion/Personas.java
+++ talleragregacion/Personas.java
@@ -28,6 +28,56 @@ public class Personas {
         this.telefono = telefono;
     }
 
+    public String getTipoDeDocumento() {
+        return tipoDeDocumento;
+    }
+
+    public void setTipoDeDocumento(String tipoDeDocumento) {
+        this.tipoDeDocumento = tipoDeDocumento;
+    }
+
+    public int getNumDocumento() {
+        return NumDocumento;
+    }
+
+    public void setNumDocumento(int NumDocumento) {
+        this.NumDocumento = NumDocumento;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+
+    
+    
     @Override
     public String toString() {
         return "Personas{" + "tipoDeDocumento=" + tipoDeDocumento + ", NumDocumento=" + NumDocumento + ", nombre=" + nombre + ", edad=" + edad + ", sexo=" + sexo + ", telefono=" + telefono + '}';
