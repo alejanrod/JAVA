75c61334d18dadcf32470fca62d441069b55f14f
alejanrod <111410298+alejanrod@users.noreply.github.com>
2023-03-06 19:39:51 -0500

    Update TallerAgregacion.java

--- talleragregacion/TallerAgregacion.java
+++ talleragregacion/TallerAgregacion.java
@@ -23,6 +23,27 @@ public class TallerAgregacion {
         System.out.println(carros);
         System.out.println(trabajadores);
         System.out.println(clientes);
+        
+        
+        
+        
+        public static void main(String[] args) {
+        
+        
+        Trabajadores trabajadores = new Trabajadores("encargado de ventas", 2500000, "8 am - 3 pm", "cedula de ciudadania", 10023933, "alejandro", 34, "masculino", 321926414);
+        Carros carros = new Carros("toyota", "cruiser", 2022, "blanco", 6, 0);
+        Clientes clientes = new Clientes("cedula de ciudadania", 234424423, "cristian", 30, "masculino", 324123);
+        Consecionario consecionario = new Consecionario( "san autos", "suba", "cll 157 # 113 c - 15", "alguien@gmail.com", 2022222,
+                                                             new Clientes("cedula de ciudadania", 234424423, "cristian", 30, "masculino", 324123),
+                                                                  new Trabajadores("encargado de ventas", 2500000, "8 am - 3 pm", "cedula de ciudadania", 10023933, "alejandro", 34, "masculino", 321926414), 
+                                                                      new Carros("toyota", "cruiser", 2022, "blanco", 6, 0));
+        System.out.println(consecionario.toString());  
+      
+        
+        System.out.println(clientes);
+    }
+        
+        
     }
     
 }
