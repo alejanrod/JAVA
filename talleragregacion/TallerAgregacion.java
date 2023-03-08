92843c16ee47690143d7b41bf03fef503c9b828e
alejanrod <111410298+alejanrod@users.noreply.github.com>
2023-03-07 19:09:53 -0500

    Update TallerAgregacion.java

--- talleragregacion/TallerAgregacion.java
+++ talleragregacion/TallerAgregacion.java
@@ -14,33 +14,21 @@ public class TallerAgregacion {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-        Consecionario consecionario = new Consecionario("San Autos", "suba", "cll 34 #113-f 80", "correo@gmail.com", 35215);
-        Trabajadores trabajadores = new Trabajadores("encargado de ventas", 2500000, "8 am - 3 pm", "cedula de ciudadania", 10023933, "alejandro", 34, "masculino", 321926414);
-        Carros carros = new Carros("toyota", "cruiser", 2022, "blanco", 6, 0);
-        Clientes clientes = new Clientes("cedula de ciudadania", 234424423, "cristian", 30, "masculino", 324123 );
-        System.out.println(consecionario);
-        System.out.println(carros);
-        System.out.println(trabajadores);
-        System.out.println(clientes);
-        
-        
-        
-        
-        public static void main(String[] args) {
-        
-        
-        Trabajadores trabajadores = new Trabajadores("encargado de ventas", 2500000, "8 am - 3 pm", "cedula de ciudadania", 10023933, "alejandro", 34, "masculino", 321926414);
-        Carros carros = new Carros("toyota", "cruiser", 2022, "blanco", 6, 0);
-        Clientes clientes = new Clientes("cedula de ciudadania", 234424423, "cristian", 30, "masculino", 324123);
+     
         Consecionario consecionario = new Consecionario( "san autos", "suba", "cll 157 # 113 c - 15", "alguien@gmail.com", 2022222,
                                                              new Clientes("cedula de ciudadania", 234424423, "cristian", 30, "masculino", 324123),
                                                                   new Trabajadores("encargado de ventas", 2500000, "8 am - 3 pm", "cedula de ciudadania", 10023933, "alejandro", 34, "masculino", 321926414), 
                                                                       new Carros("toyota", "cruiser", 2022, "blanco", 6, 0));
-        System.out.println(consecionario.toString());  
-      
+        //System.out.println(consecionario.toString());  
+        // System.out.println(clientes2);
         
-        System.out.println(clientes);
+        //sobrecarga de metodo, creamos un objeto carro condiferentes parametros
+                Carros carro = new Carros();
+            
+        carro.addcarro("mercedes", 2023, "negro");
+        carro.addcarro("toyota", "cruiser", 2020, "amarillo", 4, 23456);
+
+        System.out.println(carro);
     }
         
         
